ec26add091fea69e9916f1057e146aabd1e41c09
tkokona <koko150325@gmail.com>
2023-11-07 23:50:35 +0900

    ブログ投稿詳細画面の作成

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ use App\Http\Controllers\PostController;
 
 // Route::get('/posts',[PostController::class, 'index']);
 Route::get('/',[PostController::class, 'index']);
+Route::get('/posts/{post}',[Postcontroller::class, 'show']);
