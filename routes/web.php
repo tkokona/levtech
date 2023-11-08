a3974e43ccfa0c5a89b8b09f4145566c4ea62a7e
tkokona <koko150325@gmail.com>
2023-11-08 21:14:17 +0900

    ブログ投稿作成画面の生成

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ use App\Http\Controllers\PostController;
 
 // Route::get('/posts',[PostController::class, 'index']);
 Route::get('/',[PostController::class, 'index']);
+Route::get('/posts/create',[Postcontroller::class, 'create']);
 Route::get('/posts/{post}',[Postcontroller::class, 'show']);
