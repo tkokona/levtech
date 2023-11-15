da4af0de7b07f15282ea134b06ce7bbef7d4edf2
tkokona <koko150325@gmail.com>
2023-11-15 23:27:10 +0900

    認証機能の追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,28 +7,33 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     
-    <body>
-        <h1 class="title">Blog Name</h1>
-        <div class="content">
-            <form action="/posts/{{ $post->id }}" method="POST">
-                @csrf
-                @method('PUT')
-                <div class="content_title">
-                    <h2>タイトル</h2>
-                    <input type="text" name="post[title]" value="{{ $post->title }}"/>
-                    <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
-                </div>
-                <div class="content_body">
-                    <h2>本文</h2>
-                    <textarea name="post[body]">{{ $post->body }}</textarea>
-                    <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
-                </div>
-                <input type="submit" value="保存"/>
-            </form>
-        </div>
-       
-        <div class="back">
-            <a href="/posts/{{ $post->id }}">戻る</a>
-        </div>
-    </body>
+    <x-app-layout>
+        <x-slot name="header">
+            　Edit
+        </x-slot>
+        <body>
+            <h1 class="title">Blog Name</h1>
+            <div class="content">
+                <form action="/posts/{{ $post->id }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="content_title">
+                        <h2>タイトル</h2>
+                        <input type="text" name="post[title]" value="{{ $post->title }}"/>
+                        <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+                    </div>
+                    <div class="content_body">
+                        <h2>本文</h2>
+                        <textarea name="post[body]">{{ $post->body }}</textarea>
+                        <p class="body_error" style="color:red">{{ $errors->first('post.body') }}</p>
+                    </div>
+                    <input type="submit" value="保存"/>
+                </form>
+            </div>
+           
+            <div class="back">
+                <a href="/posts/{{ $post->id }}">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
