48fb88f80ca3d7fc72c84e48dc6d5602956c2e66
tkokona <koko150325@gmail.com>
2023-11-15 21:33:41 +0900

    カテゴリーの追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,15 +11,16 @@
         <h1 class="title">
             {{ $post->title }}
         </h1>
+        <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">編集</a>
+        </div>
         <div class='content'>
             <div class="content_post">
                 <h3>本文</h3>
                 <p class="body">{{ $post->body }}</p>
             </div>
         </div>
-        <div class="edit">
-            <a href="/posts/{{ $post->id }}/edit">編集</a>
-        </div>
+       <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
         <div class="delete"></div>
         <div class="back">
             <a href="/">戻る</a>
